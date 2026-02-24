68927d97ef52f8aaabd03fd0c2b4f0db13149a30
Md Khayrul Hasan <khayrul.web@gmail.com>
2026-02-25 05:57:11 +0600

    Update tsconfig and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@api': path.resolve(__dirname, './src/api'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@services': path.resolve(__dirname, './src/services'),
+      '@types': path.resolve(__dirname, './src/types'),
+      '@config': path.resolve(__dirname, './src/config'),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
